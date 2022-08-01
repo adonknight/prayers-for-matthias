8d965a096776cc023c33ea26504ba7c51d77dfbc
Andrew Brown <abrown.coding@gmail.com>
2022-08-01 08:57:55 -0400

    Changed the files

---
+++ index.js
@@ -0,0 +1,9 @@
+function toDark() {
+var element = document.body;
+element.classList.add("dark-mode");
+}
+
+function toLight() {
+var element = document.body;
+element.classList.remove("dark-mode");
+}
